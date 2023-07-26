ea7a1ab51e0bce17608dd7948f39169109d2395a
mastercoldprince <54665402+mastercoldprince@users.noreply.github.com>
2023-07-26 15:12:11 +0800

    230726first commit2

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set({ "n", "i" }, "<Left>", "<Nop>")
+vim.keymap.set({ "n", "i" }, "<Right>", "<Nop>")
+vim.keymap.set({ "n", "i" }, "<Up>", "<Nop>")
+vim.keymap.set({ "n", "i" }, "<Down>", "<Nop>")
---
+++ lua/plugins/HardHackerTheme.lua
@@ -0,0 +1,6 @@
+return {
+  "hardhackerlabs/theme-vim",
+  config = function()
+    vim.cmd.colorscheme("hardhacker")
+  end,
+}
